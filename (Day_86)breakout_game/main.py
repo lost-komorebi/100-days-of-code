a12d5de3abf6454a354b2bb45cce3c6f2963ba28
komorebi <caoxin19921027@gmail.com>
2022-11-16 09:33:42 +0700

    Day_86 fix cannot restart bug, add game class

--- (Day_86)breakout_game/main.py
+++ (Day_86)breakout_game/main.py
@@ -10,71 +10,75 @@ from scoreboard import ScoreBoard
 from brick_manager import BrickManager
 
 
-screen = Screen()
-screen.setup(1000, 600)
-screen.title('Breakout Game')
-screen.bgcolor('black')
-screen.tracer(0)
+class Game:
 
-paddle = Paddle()
-scoreboard = ScoreBoard()
-brick_manager = BrickManager()
-ball = Ball()
-ball.move()
+    def __init__(self):
+        self.screen = Screen()
+        self.screen.setup(1000, 600)
+        self.screen.title('Breakout Game')
+        self.screen.bgcolor('black')
+        self.screen.tracer(0)
+        self.is_game_on = True
+        self.chance = 3  # user has 3 times
+        self.paddle = Paddle()
+        self.scoreboard = ScoreBoard()
+        self.brick_manager = BrickManager()
+        self.ball = Ball()
+        self.ball.move()
+        self.check_events()
 
-is_game_on = True
-chance = 3  # user has 3 times
+    def restart(self):
+        self.is_game_on = True
+        self.chance = 3
+        self.scoreboard.reset()
+        self.ball.reset()
+        self.paddle.reset()
+        self.brick_manager.reset()
+        self.run()
+
+    def check_events(self):
+        self.screen.listen()
+        self.screen.onkey(self.paddle.move_left, 'Left')
+        self.screen.onkey(self.paddle.move_right, 'Right')
+        self.screen.onkey(self.restart, 'space')
+
+    def run(self):
+        while self.is_game_on:
+            self.screen.update()
+            self.ball.move()
+
+            # detect ball and wall(left and right) collision
+            if self.ball.xcor() >= 490 or self.ball.xcor() <= -490:
+                self.ball.change_x()
+
+            # detect ball and paddle collision
+            if self.ball.is_collision(self.paddle):
+                self.ball.change_y()
+
+            # detect ball and bricks collision
+            for brick in self.brick_manager.bricks:
+                if self.ball.is_collision(brick):
+                    # move brick out of screen, don't use hideturtle
+                    brick.goto(1000, 1000)
+                    self.ball.change_y()
+                    self.scoreboard.update_score()
+                    self.brick_manager.bricks_number -= 1
+
+            # detect ball and wall(up and down) collision
+            if self.ball.ycor() >= 280 or self.ball.ycor() <= -280:
+                self.ball.goto(0, 0)
+                self.chance -= 1
+
+            if self.chance == 0:
+                self.is_game_on = False
+                self.scoreboard.game_over()
+
+            if self.brick_manager.bricks_number == 0:
+                self.is_game_on = False
+                self.scoreboard.win()
+
+        self.screen.exitonclick()
 
 
-def restart():
-    global is_game_on
-    global chance
-    is_game_on = True
-    chance = 3
-    scoreboard.reset()
-    ball.reset()
-    brick_manager.reset()
-
-
-screen.listen()
-screen.onkey(paddle.move_left, 'Left')
-screen.onkey(paddle.move_right, 'Right')
-screen.onkey(restart, 'space')
-
-
-while is_game_on:
-    screen.update()
-    ball.move()
-
-    # detect ball and wall(left and right) collision
-    if ball.xcor() >= 490 or ball.xcor() <= -490:
-        ball.change_x()
-
-    # detect ball and paddle collision
-    if ball.is_collision(paddle):
-        ball.change_y()
-
-    # detect ball and bricks collision
-    for brick in brick_manager.bricks:
-        if ball.is_collision(brick):
-            # move brick out of screen, don't use hideturtle
-            brick.goto(1000, 1000)
-            ball.change_y()
-            scoreboard.update_score()
-            brick_manager.bricks_number -= 1
-
-    # detect ball and wall(up and down) collision
-    if ball.ycor() >= 280 or ball.ycor() <= -280:
-        ball.goto(0, 0)
-        chance -= 1
-
-    if chance == 0:
-        is_game_on = False
-        scoreboard.game_over()
-
-    if brick_manager.bricks_number == 0:
-        is_game_on = False
-        scoreboard.win()
-
-
-screen.exitonclick()
+game = Game()
+game.run()
